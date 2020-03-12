60ee9bd9512799422e973a67a298177ba5a9c2a3
ffilizz <filizikibas@gmail.com>
2020-03-11 19:31:07 -0500

    Print array

--- src/day1/Colors.java
+++ src/day1/Colors.java
@@ -1,9 +1,12 @@
 package day1;
 
+import java.util.Arrays;
+
 public class Colors {
 
     public static void main(String[] args) {
 
         String[] colors={"red","blue","green","yellow"};
+        System.out.println("colors = " + Arrays.toString(colors));
     }
 }
